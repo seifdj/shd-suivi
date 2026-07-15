76b21244c5649eeac82673aa95bbe016634d82c4
seifdj <seif.djiridi@gmail.com>
2026-07-15 18:57:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -60,3 +60,8 @@ export default defineConfig({
     })
   ]
 })
+icons: [
+  { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+  { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
+  { src: 'icon-512-maskable.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+]
